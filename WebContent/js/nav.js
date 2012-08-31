f17dbb7ef604923fd82745c872025553ddd718a9
Dr1fter <gregg@blackchicken.ca>
2012-08-30 21:17:45 -0400

    added unique titles per page

--- WebContent/js/nav.js
+++ WebContent/js/nav.js
@@ -2,11 +2,11 @@ nav = document.querySelector("#navselect");
 navoptions = nav.options;
 
 var url = document.location.href;
-console.log(url);
+//console.log(url);
 var lastSlash = url.lastIndexOf("/");
 var fileName = url.substring(lastSlash + 1, url.length);
-console.log(url);
-console.log(fileName);
+//console.log(url);
+//console.log(fileName);
 for (var i = 0; i < navoptions.length; i++) {
     if (navoptions[i].value == fileName) {
         if (navoptions.selectedIndex != i) {
